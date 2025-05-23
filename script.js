562c18be3483a4e8dc3fb49b4b225e561f8ae509
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-05-23 14:49:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let n = prompt("Please enter your name:");
 n = parseInt(n);
 let days = 0;
-if(n % 4 === 0 && n % 100 !== 0) || n % 400 === 0; {
+if((n % 4 === 0 && n % 100 !== 0) || n % 400 === 0) {
 	days = 366;
 }
 else {
 	days =365;
 }
 
-console.log(days);
+alert(days);
